b2fd2010963db7a1cc8efaf59c38372a8abbe06d
ClaireChilders <clairechilders@oakland.edu>
2023-10-31 09:21:22 -0400

    feat: define AppointmentStatuses

---
+++ Reports/AppointmentStatus.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from typing import Any
+
+
+class AppointmentStatus(Enum):
+    APPROVED = 'approved'
+    STARTED = 'started'
+    COMPLETED = 'completed'
+    NO_SHOW = 'no_show'
+    CANCELLED = 'cancelled'
+    VALID_SCHEDULED = [APPROVED, STARTED, COMPLETED]
+    VALID_COMPLETED = [STARTED, COMPLETED]
+    VALID_CANCELLED = [CANCELLED, NO_SHOW]
