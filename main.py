36a4a1d831f68e62706533c54ce4ef7cbd14b92b
ClaireChilders <clairechilders@oakland.edu>
2023-12-11 12:00:42 -0500

    feat: add message for start of log

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 
 logfile = f"logs/{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 logging.basicConfig(filename=logfile, encoding='utf-8', level=logging.DEBUG, filemode='w', format='%(levelname)s:%(asctime)s:[%(module)s] %(message)s')
+logging.info("Log started")
 
 from Utils.Config import Config
 
