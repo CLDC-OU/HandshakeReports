56aa33ca333767b444867f300a625170f67a8d0d
ClaireChilders <clairechilders@oakland.edu>
2024-01-08 14:34:46 -0500

    style: move function header comments

--- Utils/file_utils.py
+++ Utils/file_utils.py
@@ -8,11 +8,12 @@ from selenium import webdriver
 from .utils import config
 from dotenv import load_dotenv
 
-# returns the file and the directory of a file path as an ordered pair
-def split_filepath(path):
+
+def split_filepath(path):  # returns the file and the directory of a file path as an ordered pair
     directory = path.split("\\")
     filename = directory.pop()
     return "\\".join(directory), filename
+
 
 def rename_file(initial_loc, new_name):
     path = split_filepath(initial_loc)
@@ -47,16 +48,19 @@ def filter_files(file_dir, must_contain, file_type):
     logging.debug(f"found {len(valid_files)} valid files")
     return valid_files
 
+
 def replace(string, replace_arr):
     for replace_obj in replace_arr:
         string = re.sub(replace_obj["replace"], replace_obj["with"], string)
     return string
+
 
 def move_file(initial_loc, final_dir):
     filename = split_filepath(initial_loc)[1]
     shutil.move(initial_loc, final_dir + "\\" + filename)
     logging.debug(f'Moved {filename} from {initial_loc} to {final_dir}')
     return final_dir + "\\" + filename
+
 
 def move_csv(initial_dir, final_dir, filename_must_contain, replace_arr):
     if not initial_dir or not final_dir:
@@ -68,6 +72,7 @@ def move_csv(initial_dir, final_dir, filename_must_contain, replace_arr):
     updated_path = move_file(initial_dir + "\\" + file_name, final_dir)
     updated_name = replace(file_name, replace_arr)
     return rename_file(updated_path, updated_name)
+
 
 def move_files_from_config():
     files = config["files"]
