d487f0b18ddd7122091e7eccfa2214e37f57651d
ClaireChilders <clairechilders@oakland.edu>
2024-01-08 14:49:47 -0500

    refactor: create module for Column

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -8,23 +8,6 @@ from utils.df_utils import filter_target_isin, filter_target_pattern_isin, sort_
 from utils.general_utils import get_month_range
 
 
-class Column(Enum):
-    ID = 'id'
-    DATE = 'date'
-    STAFF_EMAIL = 'staff_email'
-    APPOINTMENT_TYPE = 'type'
-    STUDENT_EMAIL = 'stu_email'
-    STUDENT_COLLEGE = 'college'
-    STUDENT_MAJOR = 'major'
-    STUDENT_CLASS = 'class'
-    STUDENT_CARD_ID = 'card_id'
-    STUDENT_FIRST_NAME = 'fname'
-    STUDENT_PREFERRED_NAME = 'pref_name'
-    STUDENT_LAST_NAME = 'lname'
-    STATUS = 'status'
-    DATE_SCHEDULED = 'date_scheduled'
-    UNIQUE_REFERRAL = 'unique_referral'
-
 
 class DataSet:
     class Type(Enum):
