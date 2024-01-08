5555df75c41b67f45a27e4fc5deb1167a4c92d90
ClaireChilders <clairechilders@oakland.edu>
2024-01-08 14:35:48 -0500

    feat(get_month_range): allow no end month
    Allow no end month and add error handling for invalid input months

--- Utils/utils.py
+++ Utils/utils.py
@@ -27,10 +27,16 @@ def initialize_selenium():
     # Create a new instance of the Chrome driver
     return webdriver.Chrome(options=chrome_options)
 
+
 # Returns an array of all months between start and end inclusive
-def get_month_range(m:list):
-    if len(m) < 2:
-        return m
+def get_month_range(start_m, end_m=None) -> list[str]:
     months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-            "November", "December"]
-    return months[months.index(m[0]):months.index(m[1]) + 1]
+              "November", "December"]
+    if start_m is None or start_m not in months:
+        return []
+    if end_m is None:
+        return [months[months.index(start_m)]]
+    if end_m not in months:
+        return []
+
+    return months[months.index(start_m):months.index(end_m) + 1]
