035fc1ad1466a68c749dbbb5ebb3957ca7ac8a2f
ClaireChilders <clairechilders@oakland.edu>
2024-01-09 15:18:41 -0500

    feat: add FilterType

--- src/utils/type_utils.py
+++ src/utils/type_utils.py
@@ -1,1 +1,21 @@
 
+class FilterType():
+    def __init__(self, include: list[str] | str | None, exclude: list[str] | str | None) -> None:
+        self.include = include
+        self.exclude = exclude
+
+    @staticmethod
+    def get_set(input: str | list[str] | None) -> set[str]:
+        if input is None:
+            return set()
+        if isinstance(input, str):
+            return set(input.split(","))
+        if isinstance(input, list):
+            return set(input)
+        return set()
+
+    def get_include(self) -> set:
+        return FilterType.get_set(self.include)
+
+    def get_exclude(self) -> set:
+        return FilterType.get_set(self.exclude)
