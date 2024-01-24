7526d4ddf46ab906c590348469d7c119ae3ded31
ClaireChilders <clairechilders@oakland.edu>
2024-01-24 12:39:30 -0500

    fix: use public results property
    _results is a public property and should be renamed to results to indicate access. In the @property access methods for results, use the private _results property - only include these internal to these access methods, everywhere else should use the results property.

--- src/reports/referrals.py
+++ src/reports/referrals.py
@@ -16,7 +16,7 @@ class Referrals(Report):
         self._merge_on = merge_on
         self._appointment_cols = [appointment.get_col(Column.STUDENT_EMAIL), appointment.get_col(Column.DATE),
                                   appointment.get_col(Column.STATUS)]
-        self._results = pd.DataFrame(None)
+        self.results = pd.DataFrame(None)
 
     def run_report(self) -> None:
         self._filter_valid_appointments()
@@ -40,19 +40,19 @@ class Referrals(Report):
         self._remove_duplicates()
 
     @property
-    def _results(self) -> pd.DataFrame:
+    def results(self) -> pd.DataFrame:
         if not isinstance(self._results, pd.DataFrame):
             raise TypeError("Results is not a DataFrame")
         return self._results
 
-    @_results.setter
-    def _results(self, value: pd.DataFrame) -> None:
+    @results.setter
+    def results(self, value: pd.DataFrame) -> None:
         if not isinstance(value, pd.DataFrame):
             raise TypeError("Results is not a DataFrame")
         self._results = value
 
-    def get_results(self) -> pd.DataFrame:
-        return self._results
+    # def get_results(self) -> pd.DataFrame:
+    #     return self.results
 
     def _remove_duplicates(self):
         unique_col = self._referrals.get_col(Column.UNIQUE_REFERRAL)
@@ -61,14 +61,14 @@ class Referrals(Report):
                 "No unique col name to remove duplicates. If no unique_col is specified in files.config.json, this is expected behavior.")
             return
         logging.debug(f"Removing duplicate rows on: {unique_col}")
-        self._results.drop_duplicates(inplace=True, subset=[unique_col])
+        self.results.drop_duplicates(inplace=True, subset=[unique_col])
         logging.debug("Removed duplicate rows")
 
     def _add_scheduled(self):
-        dates = self._results[self._appointment.get_col(Column.DATE_SCHEDULED)].values.tolist()
+        dates = self.results[self._appointment.get_col(Column.DATE_SCHEDULED)].values.tolist()
         scheduled = list(map(lambda x: "FALSE" if pd.isna(x) else "TRUE", dates))
 
-        self._results.insert(
+        self.results.insert(
             loc=0,
             column="Scheduled",
             value=scheduled
@@ -78,8 +78,8 @@ class Referrals(Report):
         self._normalize_card_id()
         # make sure card ids are normalized and then merge enrollment data with it, then check that all of the correct data is there and there aren't any key errors
         if self._enrollment:
-            self._results = pd.merge(
-                left=self._results,
+            self.results = pd.merge(
+                left=self.results,
                 right=self._enrollment.get_df(),
                 on=self._merge_on,
                 how="left",
@@ -87,20 +87,20 @@ class Referrals(Report):
             )
 
     def _set_preferred_name(self):
-        pref_names = self._results[self._referrals.get_col(Column.STUDENT_PREFERRED_NAME)].values.tolist()
-        f_names = self._results[self._referrals.get_col(Column.STUDENT_FIRST_NAME)].values.tolist()
+        pref_names = self.results[self._referrals.get_col(Column.STUDENT_PREFERRED_NAME)].values.tolist()
+        f_names = self.results[self._referrals.get_col(Column.STUDENT_FIRST_NAME)].values.tolist()
         names = []
         for i in range(len(f_names)):
             names.append((pref_names[i], f_names[i]))
 
         names = list(map(lambda x: x[1] if pd.isna(x[0]) else x[0], names))
-        self._results[self._referrals.get_col(Column.STUDENT_FIRST_NAME)] = names
+        self.results[self._referrals.get_col(Column.STUDENT_FIRST_NAME)] = names
 
     def _add_completed(self):
-        statuses = self._results[self._appointment.get_col(Column.STATUS)].values.tolist()
+        statuses = self.results[self._appointment.get_col(Column.STATUS)].values.tolist()
         completed = list(map(lambda x: "TRUE" if x in AppointmentStatus.VALID_COMPLETED.value else "FALSE", statuses))
 
-        self._results.insert(
+        self.results.insert(
             loc=0,
             column="Completed",
             value=completed
@@ -110,7 +110,7 @@ class Referrals(Report):
         self._appointment.filter_appointment_type(self._valid_appointment_pattern)
 
     def _merge_referrals(self):
-        self._results = pd.merge(
+        self.results = pd.merge(
             left=self._referrals.get_df(),
             right=self._appointment.get_df(),
             how="outer",
@@ -135,35 +135,35 @@ class Referrals(Report):
         enrollment_col = self._enrollment.get_col_name(Column.STUDENT_CARD_ID)
         referral_col = self._referrals.get_col_name(Column.STUDENT_CARD_ID)
         if referral_col != enrollment_col:
-            self._results = self._results.rename(
+            self.results = self.results.rename(
                 columns={referral_col: enrollment_col}
             )
-        self._results[enrollment_col] = self._results[enrollment_col].str.replace(r'^G', '', regex=True).fillna(
+        self.results[enrollment_col] = self.results[enrollment_col].str.replace(r'^G', '', regex=True).fillna(
             -1).astype(int)
 
     def _remove_past_appointments(self):
         # first remove any null referral dates (students that had a valid appointment but not a referral)
-        self._results = self._results[~self._results[self._referrals.get_col(Column.DATE)].isnull()]
+        self.results = self.results[~self.results[self._referrals.get_col(Column.DATE)].isnull()]
         # keep students in results who had a referral after their most recent valid appointment, or have not had any valid appointment
-        self._results = self._results[~self._results[self._referrals.get_col(Column.DATE)].isna()]
+        self.results = self.results[~self.results[self._referrals.get_col(Column.DATE)].isna()]
         self._format_referral_dates()
-        self._results = self._results[~(
-            (self._results[self._appointment.get_col(Column.DATE)].dt.day_of_year < self._results[
+        self.results = self.results[~(
+            (self.results[self._appointment.get_col(Column.DATE)].dt.day_of_year < self.results[
                 self._referrals.get_col(Column.DATE)].dt.day_of_year) | (
-                    self._results[self._appointment.get_col(Column.DATE)].dt.year < self._results[
+                    self.results[self._appointment.get_col(Column.DATE)].dt.year < self.results[
                         self._appointment.get_col(Column.DATE)].dt.year)
         )]
 
     def _format_referral_dates(self):
-        self._results[self._referrals.get_col(Column.DATE)] = pd.to_datetime(
-            self._results[self._referrals.get_col(Column.DATE)]).dt.tz_localize(None)
+        self.results[self._referrals.get_col(Column.DATE)] = pd.to_datetime(
+            self.results[self._referrals.get_col(Column.DATE)]).dt.tz_localize(None)
         # self._results[self._referrals.get_col(Column.DATE)] = self._results[self._referrals.get_col(Column.DATE)].str.replace(' GMT-0400 (Eastern Daylight Time)', '')
         # self._results[self._referrals.get_col(Column.DATE)] = pd.to_datetime(self._results[self._referrals.get_col(Column.DATE)], format='%a %b %d %Y %H:%M:%S').dt.strftime('%Y-%m-%d')
 
     def _re_merge(self):
-        self._results = pd.merge(
+        self.results = pd.merge(
             left=self._referrals.get_df(),
-            right=self._results,
+            right=self.results,
             how="outer",
             on=self._referrals.get_col(Column.STUDENT_EMAIL),
             suffixes=('', '_')
@@ -175,6 +175,6 @@ class Referrals(Report):
         # get a list of unique emails in referrals
         ref_emails = self._referrals.get_df()[self._referrals.get_col_name(Column.STUDENT_EMAIL)].unique()
         # get a list of unique emails in results
-        res_emails = self._results[self._referrals.get_col_name(Column.STUDENT_EMAIL)].unique()
+        res_emails = self.results[self._referrals.get_col_name(Column.STUDENT_EMAIL)].unique()
         emails = list(set(ref_emails) - set(res_emails))
         self._referrals.get_df()[self._referrals.get_col_name(Column.STUDENT_EMAIL)][emails]
