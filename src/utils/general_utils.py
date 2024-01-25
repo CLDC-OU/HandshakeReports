d1f74d13f3137d120202eed59a08e3f9077cc81b
ClaireChilders <clairechilders@oakland.edu>
2024-01-25 17:04:54 -0500

    feat: add int_month_to_str

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -1,9 +1,11 @@
 import re
 
 
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+          "November", "December"]
+
+
 def get_month_range(start_m, end_m=None) -> list[str]:  # Returns an array of all months between start and end inclusive
-    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-              "November", "December"]
     if start_m is None or start_m not in months:
         return []
     if end_m is None:
@@ -15,6 +17,12 @@ def get_month_range(start_m, end_m=None) -> list[str]:  # Returns an array of al
         return months[months.index(start_m):] + months[:months.index(end_m) + 1]
 
     return months[months.index(start_m):months.index(end_m) + 1]
+
+
+def int_month_to_str(month: int) -> str:
+    if month < 1 or month > 12:
+        raise ValueError("Invalid month")
+    return months[month - 1]
 
 
 def list_to_regex_includes(li: set[str] | list[str] | tuple[str]) -> re.Pattern[str]:
