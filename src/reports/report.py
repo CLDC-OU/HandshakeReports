a1bb471064a2aae309c87a1ddee7a6acccde5832
ClaireChilders <clairechilders@oakland.edu>
2024-02-01 17:05:12 -0500

    feat: add colorful console logging

--- src/reports/report.py
+++ src/reports/report.py
@@ -1,6 +1,6 @@
 import logging
-
 import pandas as pd
+from colorama import Style, Fore
 
 from src.utils.df_utils import remove_columns
 from datetime import datetime as dt
@@ -40,19 +40,16 @@ class Report:
         return self.__class__.__name__
 
     def run_report(self) -> None:
-        message = f"Running {self.report.get_class_name()} report"
-        logging.debug(message)
-        print(message)
+        logging.debug(f"Running {self.report.get_class_name()} report")
+        print(f'{Fore.CYAN}Running {Fore.LIGHTYELLOW_EX}{self.report.get_class_name()}{Fore.CYAN} report{Style.RESET_ALL}')
         self.report.run_report()
         self.results = self.report.get_results()
         if self.results is None or self.results.empty:
-            message = f"\tRan {self.report.get_class_name()} report, but got no results."
-            logging.debug(message)
-            print(message)
+            logging.debug(f"\tRan {self.report.get_class_name()} report, but got no results.")
+            print(f'\t{Fore.YELLOW}Ran {Fore.LIGHTYELLOW_EX}{self.report.get_class_name()} {Fore.YELLOW}report, but got no results.{Style.RESET_ALL}')
         else:
-            message = f"\tSuccessfully ran {self.report.get_class_name()} report with {len(self.results)} results"
-            logging.debug(message)
-            print(message)
+            logging.debug(f"\tSuccessfully ran {self.report.get_class_name()} report with {len(self.results)} results")
+            print(f'\t{Fore.GREEN}Successfully ran {Fore.LIGHTYELLOW_EX}{self.report.get_class_name()} {Fore.GREEN}report with {Fore.LIGHTMAGENTA_EX}{len(self.results)} {Fore.GREEN}results{Style.RESET_ALL}')
 
     def get_results(self) -> pd.DataFrame | None:
         return None
@@ -61,6 +58,7 @@ class Report:
     def results(self) -> pd.DataFrame | None:
         if not isinstance(self._results, pd.DataFrame):
             logging.warning(f"Invalid results type {type(self._results)}. Using None")
+            print(f'{Fore.LIGHTBLACK_EX}Invalid {Fore.LIGHTWHITE_EX}results {Fore.LIGHTBLACK_EX}type {type(self._results)}. Using {Fore.LIGHTWHITE_EX}None{Style.RESET_ALL}')
             return None
         return self._results
 
@@ -72,6 +70,7 @@ class Report:
     def rename_cols(self) -> dict[str, str] | None:
         if not isinstance(self._rename_cols, dict):
             logging.warning(f"Invalid rename_cols type {type(self._rename_cols)}. Using None")
+            print(f'{Fore.LIGHTBLACK_EX}Invalid {Fore.LIGHTWHITE_EX}rename_cols {Fore.LIGHTBLACK_EX}type {type(self._rename_cols)}. Using {Fore.LIGHTWHITE_EX}None{Style.RESET_ALL}')
             return None
         return self._rename_cols
 
@@ -79,6 +78,7 @@ class Report:
     def rename_cols(self, value: dict[str, str] | None) -> None:
         if not isinstance(value, dict):
             logging.warning(f"Invalid rename_cols type {type(value)}. Using None")
+            print(f'{Fore.LIGHTBLACK_EX}Invalid {Fore.LIGHTWHITE_EX}rename_cols {Fore.LIGHTBLACK_EX}type {type(value)}. Using {Fore.LIGHTWHITE_EX}None{Style.RESET_ALL}')
             self._rename_cols = None
         self._rename_cols = value
 
@@ -88,18 +88,20 @@ class Report:
     def save_archive(self):
         if not self.archive_dir:
             logging.debug("No archive directory specified. Skipping archive")
+            print(f'{Fore.LIGHTBLACK_EX}No archive directory specified. Skipping archive{Style.RESET_ALL}')
             return
         if self.results is None or self.results.empty:
             logging.warning("No results to archive. Either this report did not have run correctly or the results were empty")
+            print(f'{Fore.LIGHTBLACK_EX}No results to archive. Either this report did not have run correctly or the results were empty{Style.RESET_ALL}')
             return
         self.results.to_csv(self.archive_dir + "\\" + self.get_filename(), index=False)
-        message = f"\tSaved archive of {self.report.get_class_name()} to {self.results_dir}\\{self.get_filename()}"
-        print(message)
-        logging.debug(message)
+        logging.debug(f"\tSaved archive of {self.report.get_class_name()} to {self.results_dir}\\{self.get_filename()}")
+        print(f'\t{Fore.LIGHTGREEN_EX}Saved archive of {Fore.LIGHTYELLOW_EX}{self.report.get_class_name()} {Fore.LIGHTGREEN_EX}to {Fore.LIGHTBLACK_EX}{self.results_dir}\\{self.get_filename()}{Style.RESET_ALL}')
 
     def save_results(self):
         if self.results is None or self.results.empty:
             logging.warning("No results to save. Either this report did not have run correctly or the results were empty")
+            print(f'{Fore.LIGHTBLACK_EX}No results to save. Either this report did not have run correctly or the results were empty{Style.RESET_ALL}')
             return
 
         if self.remove_cols:
@@ -110,6 +112,5 @@ class Report:
             self.results = self.results[self.final_cols]
 
         self.results.to_csv(self.results_dir + "\\" + self.get_filename())
-        message = f"\tSaved results of {self.report.get_class_name()} to {self.results_dir}\\{self.get_filename()}"
-        print(message)
-        logging.debug(message)
+        logging.debug(f"\tSaved results of {self.report.get_class_name()} to {self.results_dir}\\{self.get_filename()}")
+        print(f'\t{Fore.LIGHTGREEN_EX}Saved results of {Fore.LIGHTYELLOW_EX}{self.report.get_class_name()} {Fore.LIGHTGREEN_EX}to {Fore.LIGHTBLACK_EX}{self.results_dir}\\{self.get_filename()}{Style.RESET_ALL}')
