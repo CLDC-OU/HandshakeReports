e9cfb16d3a20236896cea01358dcc88813eb32f9
ClaireChilders <clairechilders@oakland.edu>
2024-02-05 14:44:06 -0500

    feat: use separate Config instances for tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,27 +4,29 @@ from src.config.config import Config
 
 
 class TestConfig(unittest.TestCase):
+    def setUp(self) -> None:
+        self.config_file = "tests/test_files/test_config.json"
+        self.config = Config(self.config_file)
+
     def test_open_config(self):
-        config = Config()
-        self.assertTrue(config._open_config())
-        self.assertTrue(config.config)
+        self.assertTrue(self.config._open_config())
+        self.assertTrue(self.config.config)
 
     def test_load_config(self):
-        config = Config()
         with patch('builtins.open') as mock_open:
             mock_open.return_value.__enter__.return_value.read.return_value = '{"key": "value"}'
-            self.assertTrue(config.load_config())
-            self.assertEqual(config.config, {"key": "value"})
+            self.assertTrue(self.config.load_config())
+            self.assertEqual(self.config.config, {"key": "value"})
 
     def test_load_config_empty_file(self):
-        config = Config()
+        config = Config("tests/test_files/empty_file.json")
         with patch('builtins.open') as mock_open:
             mock_open.return_value.__enter__.return_value.read.return_value = ''
             self.assertFalse(config.load_config())
             self.assertIsNone(config.config)
 
     def test_load_config_file_not_found(self):
-        config = Config()
+        config = Config("tests/test_files/nonexistent_file.json")
         with patch('builtins.open') as mock_open:
             mock_open.side_effect = FileNotFoundError
             self.assertFalse(config.load_config())
